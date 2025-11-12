9ddf2f7eb5ea9d5fd2afa2d3911c4f1c68e835c5
jeejocj <jeejojoshy31@gmail.com>
2025-11-12 12:57:59 +0530

    chore: setup routes and page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ai' element={<Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='write-article' element={<WriteArticle/>}/>
+        <Route path='blog-titles' element={<BlogTitles/>}/>
+        <Route path='generate-images' element={<BlogTitles/>}/>
+        <Route path='remove-background' element={<BlogTitles/>}/>
+        <Route path='remove-object' element={<BlogTitles/>}/>
+        <Route path='review-resume' element={<BlogTitles/>}/>
+        <Route path='community' element={<BlogTitles/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }

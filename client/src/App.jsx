fac198817aa05ed4cdd38ecf5275a7cd54bf24a4
jeejocj <jeejojoshy31@gmail.com>
2025-11-14 15:36:07 +0530

    bug : fixed route related bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import Layout from './pages/Layout'
 import WriteArticle from './pages/WriteArticle'
 import BlogTitles from './pages/BlogTitles'
 import Dashboard from './pages/Dashboard'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
 
 const App = () => {
   return (
@@ -14,11 +19,11 @@ const App = () => {
         <Route index element={<Dashboard/>}/>
         <Route path='write-article' element={<WriteArticle/>}/>
         <Route path='blog-titles' element={<BlogTitles/>}/>
-        <Route path='generate-images' element={<BlogTitles/>}/>
-        <Route path='remove-background' element={<BlogTitles/>}/>
-        <Route path='remove-object' element={<BlogTitles/>}/>
-        <Route path='review-resume' element={<BlogTitles/>}/>
-        <Route path='community' element={<BlogTitles/>}/>
+        <Route path='generate-images' element={<GenerateImages/>}/>
+        <Route path='remove-background' element={<RemoveBackground/>}/>
+        <Route path='remove-object' element={<RemoveObject/>}/>
+        <Route path='review-resume' element={<ReviewResume/>}/>
+        <Route path='community' element={<Community/>}/>
         </Route>
       </Routes>
     </div>

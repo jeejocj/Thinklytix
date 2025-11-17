59720ec6b401a728f9988708491e6ebb84bb6c82
jeejocj <jeejojoshy31@gmail.com>
2025-11-17 16:16:19 +0530

    bug: fixed bugs in the expand and collapse feature in the dashboard

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -20,6 +20,9 @@ const Community = () => {
       });
 
       if (data.success) {
+        // if(data.creations.length === 0){
+        //   toast.error('This feature is under Progress');
+        // }
         setCreations(data.creations);
       } else {
         toast.error(data.message);
@@ -60,48 +63,50 @@ const Community = () => {
   }, [user]);
 
   return !loading ? (
-    <div className="flex-1 h-full flex flex-col gap-4 p-6">
-      Creations
-      <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          creations.map((creation, index) => (
-            <div
-              key={creation._id || index}
-              className="relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3"
-            >
-              <img
-                src={creation.content}
-                alt=""
-                className="w-full h-full object-cover rounded-lg"
-              />
-              <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-linear-to-b from-transparent to-black/80 text-white rounded-lg">
-                <p className="text-sm hidden group-hover:block">
-                  {creation.prompt}
-                </p>
-                <div className="flex gap-1 items-center">
-                  <p>{creation.likes.length}</p>
-                  <Heart
-                    onClick={() => imageLikeToggle(creation._id)}
-                    className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${
-                      creation.likes.includes(user?.id)
-                        ? "fill-red-500 text-red-600"
-                        : "text-white"
-                    }`}
-                  />
-                </div>
+  <div className="flex-1 h-full flex flex-col gap-4 p-6">
+    Creations
+    <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
+      {creations.length === 0 ? (
+        <div className="h-full flex justify-center items-center text-gray-500 text-lg">
+          No creations
+        </div>
+      ) : (
+        creations.map((creation, index) => (
+          <div
+            key={creation._id || index}
+            className="relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3"
+          >
+            <img
+              src={creation.content}
+              alt=""
+              className="w-full h-full object-cover rounded-lg"
+            />
+            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-linear-to-b from-transparent to-black/80 text-white rounded-lg">
+              <p className="text-sm hidden group-hover:block">
+                {creation.prompt}
+              </p>
+              <div className="flex gap-1 items-center">
+                <p>{creation.likes.length}</p>
+                <Heart
+                  onClick={() => imageLikeToggle(creation._id)}
+                  className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${
+                    creation.likes.includes(user?.id)
+                      ? "fill-red-500 text-red-600"
+                      : "text-white"
+                  }`}
+                />
               </div>
             </div>
-          ))
-        )}
-      </div>
+          </div>
+        ))
+      )}
     </div>
-  ) : (
-    <div className="flex justify-center items-center h-full">
-      <span className="w-10 h-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin"></span>
-    </div>
-  );
-};
+  </div>
+) : (
+  <div className="flex justify-center items-center h-full">
+    <span className="w-10 h-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin"></span>
+  </div>
+);
+}
 
 export default Community;
